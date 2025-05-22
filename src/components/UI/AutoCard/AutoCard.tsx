9e3f4a2300f2ab7d0b630e1be119db366344a07d
sarmat <tsaloev.sarmat@gmail.com>
2025-05-22 12:12:21 +0300

    AutoCard Added

--- src/components/UI/AutoCard/AutoCard.tsx
+++ src/components/UI/AutoCard/AutoCard.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
+import "./AutoCard.scss"
 
 const AutoCard = () => {
     return (
         <div className='autoCard'>
             <img 
-                src="" 
-                alt="" 
+                className='autoCard__image'
+                src="/autoCardExample.png" 
+                alt="название машины" 
+                width={330}
+                height={300}
             />
+            <div className='autoCard__body'>
+                <div className="autoCard__carInfo">
+                    <p className='autoCard__title'>Лада 2104, 2001</p>
+                    <ul className="autoCard__carInfo-list">
+                        <li className="autoCard__carInfo-item">Объем: <b>5,5 л</b></li>
+                        <li className="autoCard__carInfo-item">Двигатель: <b>Бензин</b></li>
+                        <li className="autoCard__carInfo-item">Мощность: <b>1183 л.с.</b></li>
+                        <li className="autoCard__carInfo-item">Пробег: <b>43000</b></li>
+                    </ul>
+                </div>
+
+                <div className='autoCard__author-info'>
+                    <div className='autoCard__author-containter'>
+                        <p className="autoCard__author">Андрей</p>
+                        <div className='autoCard__location'>
+                            <img 
+                                className='autoCard__location-pin'
+                                src='/location-pin.svg'
+                                alt='местоположение'
+                                width={19}
+                                height={19}
+                            />
+                            <p className='autoCard__location-title'>Люблино</p>
+                        </div>
+                    </div>
+                    <div className="autoCard__price">
+                        <p>299 000 ₽</p>
+                    </div>
+                </div>
+
+                <div className="autoCard__like">
+                    <img 
+                        src="/heart.svg" 
+                        alt="добавить в избранное" 
+                        width={56}
+                        height={56}    
+                    />
+                </div>
+                
+            </div>
+
         </div>
     );
 }
--- src/pages/Autos/Autos.tsx
+++ src/pages/Autos/Autos.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import FindAutoForm from '../../components/UI/FindAutoForm/FindAutoForm';
+import AutoCard from '../../components/UI/AutoCard/AutoCard';
 
 const Autos = () => {
     return (
         <main className='main'>
             <FindAutoForm />
+
+            <p>Нашлось объявлений: 1</p>
+
+            <AutoCard />
         </main>
     );
 }
