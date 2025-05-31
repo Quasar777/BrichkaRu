23be33141ca7325f95ae6aa6ee7a165a16bebab5
sarmat <tsaloev.sarmat@gmail.com>
2025-05-31 19:03:02 +0300

    blocking ad creating without auth

--- src/pages/CreateAdPage/CreateAdPage.tsx
+++ src/pages/CreateAdPage/CreateAdPage.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import "./CreateAdPage.scss"
 import PageTitle from '../../components/UI/PageTitle/PageTitle';
 import CreateAdForm from '../../components/UI/CreateAdForm/CreateAdForm';
+import { AuthContext } from '../../context/AuthContext';
+import ErrorCard from '../../components/UI/ErrorCard/ErrorCard';
 
 const CreateAdPage = () => {
+    const authcontext = useContext(AuthContext);
+    if (!authcontext) {
+        return <ErrorCard errorMessage='Ошибка авторизации!'/>
+    }
+
+    const {isAuth} = authcontext;
+
+    if (!isAuth) {
+        return <ErrorCard errorMessage='Ошибка авторизации!'/>
+    }
+
     return (
         <main className='main'>
             <section className='content'>
